f6b9359a8831b4a2fda84f08469e154b89f09203
anuradha <anuradhais@gmail.com>
2019-10-25 05:20:44 +0000

    frst commit

---
+++ src/webparts/prop2Demo/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IProp2DemoWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'Prop2DemoWebPartStrings' {
+  const strings: IProp2DemoWebPartStrings;
+  export = strings;
+}
